73267f467b0dbf659db3e54dd878351144c66dac
ghosind <ghosind@gmail.com>
2024-06-26 21:55:20 +0800

    doc: update doc.

--- error.go
+++ error.go
@@ -3,6 +3,8 @@ package optional
 import "errors"
 
 var (
+	// ErrNilFunction indicates the supplier function is nil.
 	ErrNilFunction = errors.New("nil function")
+	// ErrNoSuchValue indicates the Optional does not contain any value.
 	ErrNoSuchValue = errors.New("no such value")
 )
